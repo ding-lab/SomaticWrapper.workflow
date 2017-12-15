41d115f188dc09ea6bb0e4b44608ddcc2fef47c4
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-12-14 19:39:08 -0600

    Initial commit of evaluate_status
    * merge of GDC_import and BRCA77 versions

--- src/evaluate_status.sh
+++ src/evaluate_status.sh
@@ -8,25 +8,145 @@
 # Output written to STDOUT
 
 # options
-# -f status: output only lines matching status, e.g., -f import:complete
-# -u: include only UUID in output
-# -D: include data file path in output
-# -O DATA_DIR: path to base of download directory (data in $DATA_DIR/GDC_import)
-#       Default: ./data
-# -L LOG_DIR: path to LSF logs. Default: ./bsub-logs
+# -f status: output only lines matching status, e.g., -f run_pindel:complete
+# -u: include only SN in output
+# -D DATAD: path to base of SomaticWrapper analysis directory.  Required
+# -S SCRIPTD: path to LSF logs. Required
 # -M: MGI environment.  Evaluate LSF logs 
 
-LOG_DIR="./bsub-logs"
-DATA_DIR="./data"
+
+# Look at importGDC/batch.import/evaluate_status.sh
+# for example of implementation on DC2
+
+# Its not clear how to test completion of a program in a general way, and to test if it exited with an error status
+# For now, we'll grep for "Successfully completed." in the .out file of the submitted job
+# This will probably not catch jobs which exit early for some reason (memory, etc), and is LSF specific
+
+# pass filename of log file, "completed" if finished successfully, "running" if not, and "unknown" if associated 
+# log file does not exist
+function test_LSF_success {
+# BRCA77
+LOG=$1
+
+if [ ! -e $LOG ]; then
+#>&2 echo Warning: $LOG does not seem to exist
+echo unknown
+return
+fi
+
+ERROR="Exited with exit code"
+if grep -Fq "$ERROR" $LOG; then
+    echo error
+    return
+fi
+
+SUCCESS="Successfully completed."
+if grep -Fxq "$SUCCESS" $LOG; then
+    echo completed
+else
+    echo running
+fi
+}
+
+# Usage: pass strelka bsub stderr, stdout log files
+# Return completed, running, error, or unknown
+function test_strelka_success {
+# BRCA77
+STROUT=$1
+STRERR=$2
+
+if [ ! -e $STRERR ]; then  # Error log does not exist
+#>&2 echo Warning: $F does not seem to exist
+echo unknown
+return
+fi
+
+if grep -Fq "ERROR" $STRERR; then
+    echo error
+    return
+fi
+
+test_LSF_success $STROUT
+
+}
+
+# Usage: pass pindel bsub stderr, stdout log files
+# Return completed, running, error, or unknown
+function test_pindel_success {
+# BRCA77
+STROUT=$1
+STRERR=$2
+
+# Note we are adding ad hoc tests to check for specific error conditions
+
+if [ ! -e $STRERR ]; then  # Error log does not exist
+#>&2 echo Warning: $F does not seem to exist
+echo unknown
+return
+fi
+
+if grep -Fq "disk quota exceeded" $STRERR; then
+    echo error.disk_quota
+    return
+fi
+
+if grep -Fq "Killed" $STRERR; then
+    echo error.killed
+    return
+fi
+
+test_LSF_success $STROUT
+
+}
+
+# TODO: put together a function to test if the merged vcf was written
+function test_merge_success {
+# BRCA77
+STROUT=$1
+STRERR=$2
+
+test_LSF_success $STROUT
+
+}
+
+# It is possible to devise other test for success/failure based on output in data directory
+# For instance, if Pindel LSF indicates success but data files not generated, that implies an error
+
+function get_job_status {
+# BRCA77
+SN=$1
+# evaluates status of varscan, pindel, strelka runs by checking LSF logs
+
+# Its not clear how to test completion of a program in a general way, and to test if it exited with an error status
+# For now, we'll grep for "Successfully completed." in the .out file of the submitted job
+# This will probably not catch jobs which exit early for some reason (memory, etc), and is LSF specific
+
+LOGD="$SCRIPTD/logs" 
+
+#ERRLOG="$LOGD/$SN.STEP-${STEP}.err"
+#OUTLOG="$LOGD/$SN.STEP-${STEP}.out"
+
+
+TEST1=$(test_strelka_success $LOGD/${SN}.STEP-1.out $LOGD/${SN}.STEP-1.err)  # run_strelka
+TEST2=$(test_LSF_success $LOGD/${SN}.STEP-2.out)  # run_varscan
+TEST5=$(test_pindel_success $LOGD/${SN}.STEP-5.out $LOGD/${SN}.STEP-5.err)  # run_pindel
+
+TEST3=$(test_LSF_success $LOGD/${SN}.STEP-3.out)  # parse_strelka
+TEST4=$(test_LSF_success $LOGD/${SN}.STEP-4.out)  # parse_varscan
+TEST7=$(test_LSF_success $LOGD/${SN}.STEP-7.out)  # parse_pindel
+
+TEST8=$(test_merge_success $LOGD/${SN}.STEP-8.out)  # merge_vcf
+
+TEST10=$(test_LSF_success $LOGD/${SN}.STEP-10.out)  # run_vep
+
+printf "$SN\trun_strelka:$TEST1\trun_varscan:$TEST2\trun_pindel:$TEST5\tparse_strelka:$TEST3\tparse_varscan:$TEST4\tparse_pindel:$TEST7\tmerge_vcf:$TEST8\trun_vep:$TEST10\n"
+}
 
 # http://wiki.bash-hackers.org/howto/getopts_tutorial
-while getopts ":uf:DO:L:" opt; do
+while getopts ":uf:D:S:" opt; do
   case $opt in
     u)  
-      UUID_ONLY=1
-      ;;
-    D)  
-      DATA_PATH=1
+      SN_ONLY=1
       ;;
     M)  
       MGI=1
@@ -34,11 +154,11 @@ while getopts ":uf:DO:L:" opt; do
     f) 
       FILTER=$OPTARG
       ;;
-    O) # set DATA_DIR
-      DATA_DIR="$OPTARG"
+    D) # set DATA_DIR
+      DATAD="$OPTARG"
       ;;
-    L) 
-      LOG_DIR="$OPTARG"
+    S) 
+      SCRIPTD="$OPTARG"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -54,7 +174,7 @@ shift $((OPTIND-1))
 
 if [ "$#" -ne 1 ]; then
     >&2 echo Error: Wrong number of arguments
-    >&2 echo Usage: update_batch_status.sh \[options\] batch.dat
+    >&2 echo Usage: evaluate_status.sh \[options\] batch.dat
     exit 1
 fi
 
@@ -66,114 +186,19 @@ if [ ! -e $DATD ]; then
     exit 1
 fi
 
-
-# Evaluate download status of gdc-client by examining LSF logs (MGI-specific) and existence of output file
-# Returns one of "ready", "running", "complete", "incomplete", "error"
-# Usage: test_import_success UUID FN
-# where FN is the filename (relative to data directory) as written by gdc-client
-function test_import_success {
-UUID=$1
-FN=$2
-
-LOGERR="$LOG_DIR/$UUID.err"  # this is generally not used
-LOGOUT="$LOG_DIR/$UUID.out"
-DAT="$DATD/$FN"
-DATP="$DATD/$FN.partial"
-
-# flow of gdc-client download and processing
-# 1. create output directory and $DAT.partial file as it is being downloaded
-# 2. index file (if it is a .bam) to create .bai file
-# 3. Create $DAT when it is finished.  Write "Successfully completed." to log.out file
-# 4. If dies for some reason, write "Exited with exit code" to log.out file
-
-# Other, tests may be added as necessary
-
-# If neither DAT or DATP created, assume download did not start and status is ready
-if [ ! -e $DAT ] && [ ! -e $DATP ] ; then
-    echo ready
-    return
-fi
-
-# Handle the case where LOGOUT does not exist - this might happen during some error conditions.
-# We won't know if running or error, but might establish complete or incomplete
-if [ $MGI ]; then 
-    if [ ! -e $LOGOUT ]; then
-        >&2 echo WARNING: Log file $LOGOUT does not exist.  Continuing.
-    else
-        ERROR="Exited with exit code"
-        if fgrep -Fq "$ERROR" $LOGOUT; then
-            echo error
-            return
-        fi
-
-        SUCCESS="Successfully completed."
-        if ! fgrep -Fxq "$SUCCESS" $LOGOUT; then
-            echo running
-            return
-        fi
-    fi
-fi
-
-# In case of BAM file, test if .bai file exists.  If it does, we are completed.  Otherwise, incomplete
-# We are testing for filename extension since we don't have data format information
-FNB=$(basename "$FN")
-EXT="${FNB##*.}"
-
-if [ $EXT == 'bam' ]; then
-    BAI="$DAT.bai"
-    if [ -e $BAI ]; then
-        echo complete
-    else
-        echo incomplete
-    fi
-else
-# If not BAM file, just test if result file exists
-    if [ -e $DAT ]; then
-        echo complete
-    else
-        echo incomplete
-    fi
-fi
-
-
-
-}
-
-function get_job_status {
-UUID=$1
-SN=$2
-FN=$3
-# evaluates status of import by checking LSF logs
-# Based on /gscuser/mwyczalk/projects/SomaticWrapper/SW_testing/BRCA77/2_get_runs_status.sh
-
-# Its not clear how to test completion of a program in a general way, and to test if it exited with an error status
-# For now, we'll grep for "Successfully completed." in the .out file of the submitted job
-# This will probably not catch jobs which exit early for some reason (memory, etc), and is LSF specific
-
-
-TEST1=$(test_import_success $UUID $FN)  
-
-# for multi-step processing would report back a test for each step
-printf "$UUID\t$SN\t$DATD/$FN\timport:$TEST1\n"
-}
-
 while read L; do
     # Skip comments and header
     [[ $P = \#* ]] && continue
 
-    UUID=$(echo "$L" | cut -f 1) # unique ID of file
-    SN=$(echo "$L" | cut -f 2)   # sample name
-    FN=$(echo "$L" | cut -f 3)   # filename
+    SN=$(echo "$L" | cut -f 1)   # sample name
 
-    STATUS=$(get_job_status $UUID $SN $FN )
+    STATUS=$(get_job_status $SN )
 
     # which columns to output?
-    if [ ! -z $UUID_ONLY ]; then
+    if [ ! -z $SN_ONLY ]; then
         COLS="1"
-    elif [ ! -z $DATA_PATH ]; then        
-        COLS="1-4" 
     else 
-        COLS="1,2,4" 
+        COLS="1-" 
     fi
 
     if [ ! -z $FILTER ]; then
@@ -181,6 +206,8 @@ while read L; do
     else 
         echo "$STATUS" | cut -f $COLS
     fi
+
+    exit
 
 done <$BATCH
 
